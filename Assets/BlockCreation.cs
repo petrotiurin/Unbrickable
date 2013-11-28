d7c782eda22c03a1630b00f3f4087d536ea4131a
Petro Tiurin <pt1620@my.bristol.ac.uk>
2013-11-28 22:10:07 +0000

    Fixed the bug when cubes were going through each other. Added comments.

--- Assets/BlockCreation.cs
+++ Assets/BlockCreation.cs
@@ -4,34 +4,49 @@ using System.Collections.Generic;
 
 public class BlockCreation : MonoBehaviour {
 	
-	private GameObject currentObject;
+	private GameObject currentObject; //can be used in future
     
-	private List<GameObject> objects;
+	private List<GameObject> objects; //to keep track of objects on the scene
 
 	// Use this for initialization
 	void Start () {
 		objects = new List<GameObject>();
-		currentObject = CreateCube();
-		objects.Add(currentObject);
+		CreateCube();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 	
-	public GameObject CreateCube() {
-		if (objects.Count > 3) return null;
+	/* Creates a new cube at the tob of our scene.
+	 * Works only if the last object stopped moving. */
+	public void CreateCube() {
+		//just to avoid the infinite loop.
+		if (objects.Count > 3) return;
+		//to avoid calls when object is still moving
+		if (currentObject != null &&
+			currentObject.rigidbody.isKinematic == false) return;
 		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.transform.position = new Vector3(0, 0, -2);
-		cube.transform.localScale = new Vector3(1, 1, 1);
+		addToScene(cube);
+		cube.transform.position = new Vector3(0, 0, -5); //TODO: randomize?
+		//cube.transform.localScale = new Vector3(1, 1, 1);
 		cube.AddComponent("BlockMovement");
-		BoxCollider c = (BoxCollider)cube.GetComponent("BoxCollider");
-		c.isTrigger = true;
 		Rigidbody cubeRigidBody = cube.AddComponent<Rigidbody>();
 		cubeRigidBody.mass = 2;
 		cubeRigidBody.useGravity = true;
 		currentObject = cube;
-		objects.Add(currentObject);
-		return cube;
+		objects.Add(cube);
+	}
+	
+	//for future use
+	public GameObject getCurrentObject(){
+		return currentObject;
+	}
+	
+	//Makes given object a child of the "Scene"
+	private void addToScene(GameObject o){
+		GameObject scene = GameObject.Find("Scene");
+		Transform t = o.transform;
+		t.parent = (Transform)scene.GetComponent("Transform");
 	}
 }
--- Assets/BlockMovement.cs
+++ Assets/BlockMovement.cs
@@ -3,48 +3,21 @@ using System.Collections;
 
 public class BlockMovement : MonoBehaviour {
 	
-	
-	private System.Diagnostics.Stopwatch stopwatch;
-	private bool moving = true;
-	
 	// Use this for initialization
 	void Start () {
-		stopwatch = new System.Diagnostics.Stopwatch();
-		stopwatch.Start();
 	}
 	
 	
 	// Update is called once per frame
 	void Update () {
-		if (moving && stopwatch.ElapsedMilliseconds > 15*60){
-			this.transform.Translate(0, -1, 0);
-			stopwatch.Reset();
-			stopwatch.Start();
-		}
 	}
 	
-	void OnTriggerEnter(Collider other) {
-		rigidbody.useGravity = false;
+	/* On collision - send a request to the
+	 * BlockCreation script to create a new block */
+	void OnCollisionEnter(Collision collision) {
 		rigidbody.isKinematic = true;
-        Stop();
 		GameObject scene = GameObject.Find("Scene");
     	BlockCreation script = (BlockCreation) scene.GetComponent(typeof(BlockCreation));
 		script.CreateCube();
     }
-	
-	void OnTriggerStay (Collider other)
-    {  
-		
-    }
-        
-  	void OnTriggerExit (Collider other)
-    {
-     
-    }
-	
-	void Stop(){
-		moving = false;
-		stopwatch.Stop();
-		stopwatch.Reset();
-	}
 }
