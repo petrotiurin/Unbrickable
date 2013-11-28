ff9972948ce401672053c3037976fc9dc80ea293
Petro Tiurin <pt1620@my.bristol.ac.uk>
2013-11-28 11:26:20 +0000

    Couple of scripts for block creation. Needs fixing.

---
+++ Assets/BlockCreation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockCreation : MonoBehaviour {
+	
+	private GameObject currentObject;
+    
+
+	// Use this for initialization
+	void Start () {
+		currentObject = CreateCube();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	public GameObject CreateCube() {
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = new Vector3(0, 0, -2);
+		cube.transform.localScale = new Vector3(1, 1, 1);
+		cube.AddComponent("BlockMovement");
+		BoxCollider c = (BoxCollider)cube.GetComponent("BoxCollider");
+		c.isTrigger = true;
+		Rigidbody cubeRigidBody = cube.AddComponent<Rigidbody>();
+		cubeRigidBody.mass = 2;
+		cubeRigidBody.useGravity = true;
+		return cube;
+	}
+}
---
+++ Assets/BlockMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMovement : MonoBehaviour {
+	
+	
+	private System.Diagnostics.Stopwatch stopwatch;
+	private bool moving = true;
+	
+	// Use this for initialization
+	void Start () {
+		stopwatch = new System.Diagnostics.Stopwatch();
+		stopwatch.Start();
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving && stopwatch.ElapsedMilliseconds > 10*60){
+			this.transform.Translate(0, -1, 0);
+			stopwatch.Reset();
+			stopwatch.Start();
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		rigidbody.useGravity = false;
+		rigidbody.isKinematic = true;
+        Stop();
+		GameObject scene = GameObject.Find("Scene");
+    	BlockCreation script = (BlockCreation) scene.GetComponent(typeof(BlockCreation));
+		script.CreateCube();
+    }
+	
+	void OnTriggerStay (Collider other)
+    {  
+		
+    }
+        
+  	void OnTriggerExit (Collider other)
+    {
+     
+    }
+	
+	void Stop(){
+		moving = false;
+		stopwatch.Stop();
+		stopwatch.Reset();
+	}
+}
