bd18d0c0ebf10fe6c5c42feba1ad58b0f384eacc
rm1381 <rm1381@my.bristol.ac.uk>
2013-12-03 16:37:49 +0000

    Board now has relationship with blocks + Layer completion
    There is now an array representation of the board spaces, which holds
    the 'block' objects (instead of a list). This way you can quickly access
    a block at a given position on the board. The blocks fall in a
    contiguous manner to cover each space on the board. When a layer fills
    up it is destroyed. (At the moment only the bottom layer of bricks can
    be destroyed).

---
+++ Assets/BlockCollision.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockCollision : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	/* On collision - send a request to the
+	 * BlockCreation script to create a new block */
+	void OnCollisionEnter(Collision collision) {
+		int y;
+		
+		//Only collisions with the base register for now
+		if (collision.collider.gameObject.name == "base"){	/* TODO: collisions register with TOP of   */
+			rigidbody.isKinematic = true;					/* other blocks also (not sides of blocks) */
+			
+			GameObject scene = GameObject.Find("Scene");
+	    	BlockControl script = (BlockControl) scene.GetComponent(typeof(BlockControl));
+			Board gameBoard = script.getBoard();
+			
+			//get height and add to the layer count
+			y = (int) rigidbody.position.y;
+			gameBoard.FillPosition(y);
+			
+			//destroy the layer if it is full
+			if(gameBoard.layer[y] == gameBoard.nx*gameBoard.nz){
+				gameBoard.clearLayer(y);
+				script.removeBricks(y);
+			}
+			
+			script.CreateCube();
+		}
+    }
+	
+}
---
+++ Assets/BlockControl.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlockControl : MonoBehaviour {
+	
+	private GameObject[] bricks;
+	
+	private Board gameBoard = new Board();
+	
+	private int i, j;
+	
+	private GameObject FragmentCube;
+
+	// Use this for initialization
+	void Start () {
+		bricks = new GameObject[gameBoard.nx*gameBoard.ny*gameBoard.nz];
+		i = 0;
+		j = 0;
+		CreateCube();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	
+	/* Creates a new cube at the top of our scene.
+	 * Works only if the last object stopped moving. */
+	public void CreateCube() {
+		int k;
+		
+		//wrap-around
+		if (i == 5){
+			i = 0;
+			j = (j+1)%5;
+		}
+		
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.name = "block";
+		addToScene(cube);
+		
+		//drop a brick on each space on the board in order
+		cube.transform.position = new Vector3(i, 6, j); //TODO: randomize?
+		
+		cube.AddComponent("BlockCollision");
+		Rigidbody cubeRigidBody = cube.AddComponent<Rigidbody>();
+		cubeRigidBody.mass = 1;
+		cubeRigidBody.useGravity = true;
+		
+		//add brick to array representation of board.
+		k = i;
+		addBrick(k, j, cube);
+			
+		i++;
+	}
+	
+	public Board getBoard(){
+		return gameBoard;
+	}
+	
+	//Makes given object a child of the "Scene"
+	private void addToScene(GameObject o){
+		GameObject scene = GameObject.Find("Scene");
+		Transform t = o.transform;
+		t.parent = (Transform)scene.GetComponent("Transform");
+	}
+	
+	private void addBrick(int x, int z, GameObject cube){
+		if (bricks[x + z*gameBoard.nz] == null){
+			print ("brick added to bricks["+(x+z*gameBoard.nz)+"]");
+			bricks[x + z*gameBoard.nz] = cube;
+		}
+		else{
+			x += gameBoard.nx*gameBoard.nz;
+			addBrick(x, z, cube);
+		}
+		return;
+	}
+	
+	public void removeBricks(int y){
+		int x = gameBoard.nx;
+		int z = gameBoard.nz;
+		for (int k = y*x*z; k<((y+1)*x*z); k++){
+			if (bricks[k] != null){
+				Destroy (bricks[k]);
+				bricks[k] = null;
+			}
+		}
+		return;
+	}
+	
+	
+}
