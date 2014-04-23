d7ff90d8a31387d9ec94ee471abf1f7d8c64fc21
guyhowcroft <guyhowcroft@yahoo.co.uk>
2014-04-23 15:27:22 +0100

    vision all works

--- Assets/BlockControl.cs
+++ Assets/BlockControl.cs
@@ -845,12 +845,12 @@ public class BlockControl : MonoBehaviour {
 		int hello = main ();
 
 		StartCoroutine(Wait2(3));
-	//	string legoCode = Load("/Users/guyhowcroft/Documents/gameImages/result.txt");
+		string legoCode = Load("/Users/guyhowcroft/Documents/gameImages/result.txt");
 		StartCoroutine(Wait2(1));
-//		print (legoCode);
-	//	shape4 = getShapeArray(legoCode);    //If your using the webcams to get the shape
+		print (legoCode);
+		shape4 = getShapeArray(legoCode);    //If your using the webcams to get the shape
 	
-		shape4 = getShapeArray();   //If your using a hardcoded shape
+	//	shape4 = getShapeArray();   //If your using a hardcoded shape
 	
 		createShape(shape4);
 	}
