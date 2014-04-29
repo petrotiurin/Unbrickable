8e16afd39674501e70d52bed6c5b6748bef0abd4
Aankhi Mukherjee <mukherjee.aankhi@gmail.com>
2014-04-29 11:14:21 +0100

    Press enter to drop first piece
    Timer starts a bit late if you start on Scene 1, is fine starting from
    MainMenu. Going to add the timer to give the player time to get ready
    next.

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -21,10 +21,6 @@ public class Board : MonoBehaviour {
     bool countdown = false;
     bool pieceSuggestor = false;
 
-    //Make shape fall when enter is pressed
-    bool shapeFalling = false;
-    int xTime = 0;
-
 	//timer progress bar
     public float barDisplay = 1; //current progress
     public Vector2 pos = new Vector2(50,100);
@@ -144,11 +140,7 @@ public class Board : MonoBehaviour {
 	
 	// Update is called once per frame.
 	void Update (){
-		if(Input.GetKeyDown("return")){
-            shapeFalling = true;
-            xTime = 999999999;
-            Debug.Log ("ENTER");
-        }
+		
 	}
 	
 	// Create the base of the game board.
@@ -338,16 +330,9 @@ public class Board : MonoBehaviour {
 	**   rotate the board, if you need to see where you'd place the blocks.
 	*/
 	IEnumerator Wait(){
-		pauseGame(Time.realtimeSinceStartup);
+		pauseGame(0);//Time.realtimeSinceStartup);
         Debug.Log("Wait for " + timeGap + "s");
-
-        xTime = 0;
-
-        while(xTime < (timeGap / 0.01)){
-            yield return new WaitForSeconds(0.01f);
-            xTime++;
-        }
-
+        yield return new WaitForSeconds(timeGap);
         Debug.Log("After waiting for " + timeGap + "s");
 		unpauseGame();
 		blockCtrl.createShape();
@@ -420,6 +405,7 @@ public class Board : MonoBehaviour {
 
         // The following lines of code deals with the countdown timers.
         if(countdown){
+
             float timediff = Time.realtimeSinceStartup - starttimer;
             if(timediff < timeGap){
                 //Debug.Log("timediff = " + timediff);
